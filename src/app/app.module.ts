7433e391b0ccb057aad545135a1fcff6613b98df
Kevin Wu Won <exclipy@gmail.com>
2017-12-23 13:28:31 +1000

    Push logic into Viewer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
-import { IconComponent } from './icon/icon.component';
+import {IconComponent} from './icon/icon.component';
+import {ViewerComponent} from './viewer/viewer.component';
 
+const appRoutes: Routes = [
+  {path: '', component: ViewerComponent},
+];
 
 @NgModule({
-  declarations: [AppComponent, IconComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, IconComponent, ViewerComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
