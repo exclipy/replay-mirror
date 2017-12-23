0fcd7e0aff3bc5b98c2f9fb0f66cc10cddc08a9f
Kevin Wu Won <exclipy@gmail.com>
2017-12-23 13:59:04 +1000

    Add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
 import {IconComponent} from './icon/icon.component';
 import {ViewerComponent} from './viewer/viewer.component';
 
 const appRoutes: Routes = [
-  {path: '', component: ViewerComponent},
+  {path: '', component: HomeComponent},
+  {path: 'run', component: ViewerComponent},
 ];
 
 @NgModule({
-  declarations: [AppComponent, IconComponent, ViewerComponent],
+  declarations: [AppComponent, IconComponent, ViewerComponent, HomeComponent],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/icon/icon.component.ts
+++ src/app/icon/icon.component.ts
@@ -3,6 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'app-icon',
   templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.css'],
 })
 export class IconComponent {
   @Input() pic: string;
