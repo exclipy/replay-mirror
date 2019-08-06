caab4570ba124ca514ceda4c4175206fde7512d2
Kevin Wu Won <exclipy@gmail.com>
2019-08-05 21:41:53 -0400

    Hard coding home page for better load time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import {NavigationEnd, Router} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   constructor(@Inject(Router) router: Router) {
