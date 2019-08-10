4be0fcc0166bec7b164a1d07fb3f5ed60e85153f
Kevin Wu Won <exclipy@gmail.com>
2019-08-10 18:30:40 -0400

    Tweak button styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import {BrowserParamsService} from '../browser-params.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnDestroy {
   isUnsupportedBrowser = false;
